cae290db03e964fd45a4162eb573e388f276daec
Michelangelo Ruini <michelangelo@MBPdiMielangelo.wind3.hub>
2026-07-03 13:56:23 +0200

    Added first six campaigns

--- data/campaigns.js
+++ data/campaigns.js
@@ -70,5 +70,152 @@ const campaigns = [
       "05.jpg",
       "video-01.mp4"
     ]
-  }
+  },
+
+  {
+  id: "liberty-2025",
+  client: "Liberty",
+  title: "150th Anniversary 2025",
+  category: "Campaigns",
+  path: "assets/campaigns/liberty-2025/",
+  layout: "info-left",
+
+  border: true,
+
+  credits: [
+    {
+      label: "Photographer",
+      value: "Dusan Reljin"
+    },
+    {
+      label: "Models",
+      value: [
+        "Yasmin Le Bon"
+      ]
+    }
+  ],
+
+  media: [
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "05.jpg",
+    "video-01.mp4"
+  ]
+},
+
+{
+  id: "marina-rinaldi-ss25",
+  client: "Marina Rinaldi",
+  title: "SS 2025",
+  category: "Campaigns",
+  path: "assets/campaigns/marina-rinaldi-ss25/",
+  layout: "info-right",
+
+  border: true,
+
+  credits: [
+    {
+      label: "Photographer",
+      value: "Julia Noni"
+    },
+    {
+      label: "Models",
+      value: [
+        "Sofia Klimkowski",
+        "Celina Ralph"
+      ]
+    }
+  ],
+
+  media: [
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "05.jpg",
+    "video-01.mp4"
+  ]
+},
+
+{
+  id: "chantecler-2024",
+  client: "Chantecler",
+  title: "2024",
+  category: "Campaigns",
+  path: "assets/campaigns/chantecler-2024/",
+  layout: "info-left",
+
+  border: false,
+
+  credits: [
+    {
+      label: "Photographer",
+      value: "Federico De Angelis"
+    },
+    {
+      label: "Models",
+      value: [
+        "Greta Varese",
+        "Masha Zaytseva"
+      ]
+    }
+  ],
+
+  media: [
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "05.jpg",
+    "06.jpg",
+    "07.jpg",
+    "08.jpg",
+    "09.jpg",
+    "10.jpg",
+    "11.jpg",
+    "video-01.mp4"
+  ]
+},
+
+{
+  id: "intimissimi-ss24",
+  client: "Intimissimi",
+  title: "SS 2024",
+  category: "Campaigns",
+  path: "assets/campaigns/intimissimi-ss24/",
+  layout: "info-right",
+
+  border: false,
+
+  credits: [
+    {
+      label: "Photographer",
+      value: "Norman Jean Roy"
+    },
+    {
+      label: "Talent",
+      value: "Jennifer Lopez"
+    },
+    {
+      label: "Director",
+      value: "Jacob Bixenman"
+    }
+  ],
+
+  media: [
+    "01.jpg",
+    "02.jpg",
+    "03.jpg",
+    "04.jpg",
+    "05.jpg",
+    "06.jpg",
+    "07.jpg",
+    "08.jpg",
+    "09.jpg",
+    "10.jpg",
+    "video-01.mp4"
+  ]
+}
 ];
